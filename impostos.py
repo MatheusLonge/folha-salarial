cbd725c06e539524875b5070d0946fe4d62a50bb
MatheusLonge <m.longe.dev@gmail.com>
2025-07-03 15:13:03 -0300

    Update impostos.py
    Fazendo testes quanto a chamada da função e uso da variável no escopo local e global que será visto no próximo commit

--- impostos.py
+++ impostos.py
@@ -1,20 +1,22 @@
-def irf(n): # Imposto de Renda
-    if n <= 2428.81 :
-        aliquota = 0.0
-        deducao  = 0.0
-    elif n <= 2826.66:
-        aliquota = 0.075
-        deducao  = 182.16
-    elif n <= 3751.06:
-        aliquota = 0.15
-        deducao  = 
-    elif n <= 4664.68:
-        aliquota=0.225
-    elif n > 4664.68:
-        aliquota=0.275
-    else:
-        print('Valor inválido')
-    resultado = n / aliquota
-    print(resultado)
-irf(5000)
-
+def irf(valores): # Imposto de Renda
+        valores = float(input("Digite o salário bruto: "))
+        if valores <= 2428.81 :
+            aliquota = 0.0
+            deducao  = 0.0
+        elif valores <= 2826.66:
+            aliquota = 0.075
+            deducao  = 182.16
+        elif valores <= 3751.06:
+            aliquota = 0.15
+            deducao  = 394.16
+        elif valores <= 4664.68:
+            aliquota = 0.225
+            deducao  = 675.49  
+        elif valores > 4664.68:
+            aliquota = 0.275
+            deducao  = 908.73
+        else:
+            print('Valor inválido')
+        resultado = valores * aliquota - deducao
+        print("O imposto de renda a ser pago é de R$", resultado)
+irf(0)
