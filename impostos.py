f6604a50b04046e0fccac239ec1c785038206dd2
MatheusLonge <m.longe.dev@gmail.com>
2025-07-03 15:38:52 -0300

    Update impostos.py
    inclusão do inss como função

--- impostos.py
+++ impostos.py
@@ -20,3 +20,20 @@ def irf(valores): # Imposto de Renda
         resultado = valores * aliquota - deducao
         print("O imposto de renda a ser pago é de R$", round(resultado, ndigits=2)
 irf(0)
+def inss(salario):
+    salario = float(input("Digite o salário bruto: "))
+    if salario <= 1518 :
+         aliquota = 0.075
+         deducao  = 0.0
+    if salario <= 2793.88 :
+         aliquota = 0.09
+         deducao  = 22.77
+    if salario <= 4190.83:
+         aliquota = 0.12
+         deducao  = 106.59
+    if salario >= 8157.41:
+         aliquota = 0.14
+         deducao  = 190.40
+    resultado = salario * aliquota -  deducao
+    print("Será descontado R$", round(resultado, ndigits=2), "de inss")
+inss(0)
