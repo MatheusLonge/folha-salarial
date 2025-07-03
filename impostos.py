85ebdcfca09a2c6962e5cab6f86c3dae2870804f
MatheusLonge <m.longe.dev@gmail.com>
2025-07-03 15:41:27 -0300

    Update impostos.py
    Como os argumentos fazem parte de escopo local, achei melhor colocar com o mesmo nome o argumento, visto que usarão como base o mesmo valor

--- impostos.py
+++ impostos.py
@@ -1,24 +1,24 @@
-def irf(valores): # Imposto de Renda
-        valores = float(input("Digite o salário bruto: "))
-        if valores <= 2428.81 :
+def irf(salario): # Imposto de Renda
+        salario = float(input("Digite o salário bruto: "))
+        if salario <= 2428.81 :
             aliquota = 0.0
             deducao  = 0.0
-        elif valores <= 2826.66:
+        elif salario <= 2826.66:
             aliquota = 0.075
             deducao  = 182.16
-        elif valores <= 3751.06:
+        elif salario <= 3751.06:
             aliquota = 0.15
             deducao  = 394.16
-        elif valores <= 4664.68:
+        elif salario <= 4664.68:
             aliquota = 0.225
             deducao  = 675.49  
-        elif valores > 4664.68:
+        elif salario > 4664.68:
             aliquota = 0.275
             deducao  = 908.73
         else:
             print('Valor inválido')
-        resultado = valores * aliquota - deducao
-        print("O imposto de renda a ser pago é de R$", round(resultado, ndigits=2)
+        resultado = salario * aliquota - deducao
+        print("O imposto de renda a ser pago é de R$", round(resultado, ndigits=2))
 irf(0)
 def inss(salario):
     salario = float(input("Digite o salário bruto: "))
